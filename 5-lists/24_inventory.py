38d82c9e6340b4650a217b8e1aec5cace51e75d9
John Emmanuel C. Villegas <johnemmanuel.villegas20@gmail.com>
2024-07-20 15:19:14 +0800

    Add files via upload

---
+++ 5-lists/24_inventory.py
@@ -0,0 +1,7 @@
+# Inventory 📦
+# Codédex
+
+airplane_toys = [ 898, 732, 543, 878 ]
+
+print(min(airplane_toys))
+print(max(airplane_toys))
---
+++ 5-lists/25_reading_list.py
@@ -0,0 +1,16 @@
+# Reading List 📚
+# Codédex
+
+books = ['Zero to One',
+         'The Lean Startup',
+         'The Mom Test',
+         'Made to Stick',
+         'Life in Code']
+
+print(books)
+
+books.append('Zero to Sold')
+books.remove('Zero to One')
+books.pop(0)
+
+print(books)
